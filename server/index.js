c6cbcfed965a120b76e33d4324a0be457bde103f
Blue Endaya <blue.endaya@gmail.com>
2023-08-23 19:22:13 +0800

    backend server connected to db

--- server/index.js
+++ server/index.js
@@ -6,23 +6,23 @@ require("dotenv").config();
 const { MONGO_URL, PORT } = process.env;
 
 mongoose
-    .connect(MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    })
-    .then(() => console.log("MongoDB is connected successfully"))
-    .catch((err) => console.error(err));
+  .connect(MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB is  connected successfully"))
+  .catch((err) => console.error(err));
 
-app.listen.apply(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is listening on port ${PORT}`);
 });
 
 app.use(
-    cors({
-        origin: ["http://localhost:4000"],
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        credentials: true,
-    })
+  cors({
+    origin: ["http://localhost:4000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
 );
 
 app.use(express.json());
